b879b7093a4fd3690b0831e9a4dff1460c340f95
Serena <121532151+serena-sang@users.noreply.github.com>
2025-05-22 15:20:39 +0000

    Chapter 3 challenge answer

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -3,6 +3,12 @@ package com.example.explorecalijpa.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.explorecalijpa.model.Tour;
+import com.example.explorecalijpa.model.Difficulty;
+import com.example.explorecalijpa.model.TourPackage;
+import java.util.List;
+
 
 public interface TourRepository extends JpaRepository<Tour, Integer> {
+  List<Tour> findByDifficulty(Difficulty difficulty);
+  List<Tour> findByTourPackage(String code);
 }
