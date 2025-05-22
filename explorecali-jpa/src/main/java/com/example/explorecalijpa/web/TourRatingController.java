d0b30c2ef4e17da536207caf0aa28a330c36506f
Serena <121532151+serena-sang@users.noreply.github.com>
2025-05-22 16:56:19 +0000

    Create http get endpoint

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -1,9 +1,12 @@
 package com.example.explorecalijpa.web;
 
+import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +15,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.explorecalijpa.business.TourRatingService;
+import com.example.explorecalijpa.model.TourRating;
 
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Tour Rating Controller
@@ -40,6 +45,17 @@ public class TourRatingController {
   public void createTourRating(@PathVariable(value = "tourId") int tourId,
       @RequestBody @Valid RatingDto ratingDto) {
     tourRatingService.createNew(tourId, ratingDto.getCustomerId(), ratingDto.getScore(), ratingDto.getComment());
+  }
+
+  @GetMapping
+  public List<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
+    List<TourRating> tourRatings = tourRatingService.lookupRatings(tourId);
+    return tourRatings.stream().map(RatingDto::new).toList();
+  }
+
+  @GetMapping("/average")
+  public Map<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
+    return Map.of("average", tourRatingService.getAverageScore(tourId));
   }
 
   @ExceptionHandler(NoSuchElementException.class)
